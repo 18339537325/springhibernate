553451b1ab22cb3a46d34aa4450a0773e8b701b0
18339537325 <249744893@qq.com>
2018-09-13 10:48:19 +0800

    kjhl

---
+++ StudentController.java
@@ -0,0 +1,22 @@
+package com.Controller;
+
+import com.Service.StudentService;
+import com.entity.StudentEntity;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/get")
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+    @RequestMapping("/get.do")
+    public String getStudent(int id){
+        StudentEntity entity = studentService.findById(1);
+        Gson gson = new Gson();
+        String json = gson.toJson(entity);
+        return json;
+    }
+}
